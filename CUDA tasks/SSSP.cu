48a8ef8a421bc53de91ff410e7ea06e364270123
Hrishikesh Bodkhe <hrishikesh.b9315@gmail.com>
2024-01-21 17:25:00 +0000

    Added Worklist based SSSP

--- CUDA tasks/SSSP.cu
+++ CUDA tasks/SSSP.cu
@@ -1,6 +1,10 @@
 #include<cuda.h>
 #include "preprocessing.h"
 
+//__device__ float *idx;
+
+void ssspWorklist(ll totalVertices, ll *dindex, ll *dheadvertex, ll *dweights);
+
 void ssspVertexCentric(ll totalVertices, ll *dindex, ll *dheadvertex, ll *dweights);
 
 void ssspEdgeCentric(ll totalVertices, ll totalEdges, ll *src, ll *dest, ll *weights);
@@ -8,6 +12,53 @@ void ssspEdgeCentric(ll totalVertices, ll totalEdges, ll *src, ll *dest, ll *wei
 void buildCSR(ll vertices, ll edges, vector<Edge>& edgelist, ll *index, ll *headvertex, ll *weights, unordered_map<ll, ll>& degrees);
 
 void buildCOO(ll edges, vector<Edge>& edgelist, ll *src, ll *dest, ll *weights);
+
+__global__ void ssspWorklistKernel(ll workers, ll *dindex, ll *dheadvertex, ll *dweights, ll *curr, ll *next, ll *dist, float *idx){
+    unsigned id = blockIdx.x * blockDim.x + threadIdx.x;
+
+    if(id >= workers) return;
+//    printf("id: %d. Here 1\n", id);
+
+    ll u = curr[id];
+    ll start = dindex[u];
+//    printf("start: %ld \n", start);
+    ll end = dindex[u + 1];
+//    printf("end: %ld \n", end);
+//    printf("Here 2\n");
+
+    for(ll i = start; i < end; ++i){
+        ll v = dheadvertex[i];
+        ll wt = dweights[i];
+//        printf("Here 3\n");
+        if(dist[v] > dist[u] + wt){
+            atomicMin(&dist[v], dist[u] + wt);
+            ll index = atomicAdd(idx, 1);
+
+            next[index] = v;
+//            printf("v: %ld, next[%ld]: %ld\n", v, index, next[index]);
+//            printf("Here 4\n");
+        }
+    }
+}
+
+__global__ void swapWorklist(ll *curr, ll *next, ll idx){
+//    printf("here");
+    ll *temp = curr;
+    curr = next;
+    next = temp;
+
+    printf("idx: %ld\n", idx);
+
+    for(ll i = 0; i < idx; ++i) printf("%ld ", curr[i]);
+    printf("\n");
+}
+
+__global__ void setIndexForWorklist(float *idx){
+//    printf("Entered\n");
+    *idx = 0;
+//    printf("Leaving\n");
+    return;
+}
 
 __global__ void ssspEdgeCall(ll totalEdges, ll *dsrc, ll *ddest, ll *dweights, ll *dist, int *ddchanged){
     unsigned id = blockIdx.x * blockDim.x + threadIdx.x;
@@ -58,6 +109,11 @@ __global__ void ssspVertexInit(ll vertices, ll *dist){
 
 __global__ void initSrc(ll src, ll *dist){
     dist[src] = 0;
+}
+
+__global__ void init(ll src, ll *dist, ll *curr){
+    dist[src] = 0;
+    curr[0] = src;
 }
 
 __global__ void printDist(ll vertices, ll *dist){
@@ -166,6 +222,7 @@ int main(){
     cout << "What do you want to compute?" << endl;
     cout << "1. Vertex-Based SSSP" << endl;
     cout << "2. Edge-Based SSSP" << endl;
+    cout << "3. Worklist-Based SSSP" << endl;
 
     int algoChoice;
     cin >> algoChoice;
@@ -252,7 +309,7 @@ int main(){
     ll *hheadvertex;
     ll *hweights;
 
-    if(algoChoice == 1) hindex = (ll *)malloc((totalVertices + 1) * sizeof(ll));
+    if(algoChoice == 1 || algoChoice == 3) hindex = (ll *)malloc((totalVertices + 1) * sizeof(ll));
     else if(algoChoice == 2) hsrc = (ll *)malloc((totalEdges) * sizeof (ll));
     hheadvertex = (ll *)malloc(totalEdges * sizeof(ll));
     hweights = (ll *)malloc(totalEdges * sizeof(ll));
@@ -261,7 +318,7 @@ int main(){
     cudaMemGetInfo(&initialFreeMemory, &totalMemory);
     cout << "Initial Free Memory: " << initialFreeMemory / (1024 * 1024 * 1024) << " GB" << endl;
 
-    if(algoChoice == 1) buildCSR(totalVertices, totalEdges, edgeList, hindex, hheadvertex, hweights, degrees);
+    if(algoChoice == 1 || algoChoice == 3) buildCSR(totalVertices, totalEdges, edgeList, hindex, hheadvertex, hweights, degrees);
     else if(algoChoice == 2) buildCOO(totalEdges, edgeList, hsrc, hheadvertex, hweights);
 
     ll *dindex;
@@ -269,12 +326,12 @@ int main(){
     ll *dheadVertex;
     ll *dweights;
 
-    if(algoChoice == 1) cudaMalloc(&dindex, (ll)(totalVertices + 1) * sizeof(ll));
+    if(algoChoice == 1 || algoChoice == 3) cudaMalloc(&dindex, (ll)(totalVertices + 1) * sizeof(ll));
     else if(algoChoice == 2) cudaMalloc(&dsrc, (ll)(totalEdges) * sizeof(ll));
     cudaMalloc(&dheadVertex, (ll)(totalEdges) * sizeof(ll));
     cudaMalloc(&dweights, (ll)(totalEdges) * sizeof(ll));
 
-    if(algoChoice == 1) cudaMemcpy(dindex, hindex, (ll)(totalVertices + 1) * sizeof(ll), cudaMemcpyHostToDevice);
+    if(algoChoice == 1 || algoChoice == 3) cudaMemcpy(dindex, hindex, (ll)(totalVertices + 1) * sizeof(ll), cudaMemcpyHostToDevice);
     else if(algoChoice == 2) cudaMemcpy(dsrc, hsrc, (ll)(totalEdges) * sizeof(ll), cudaMemcpyHostToDevice);
     cudaMemcpy(dheadVertex, hheadvertex, (ll)(totalEdges) * sizeof(ll), cudaMemcpyHostToDevice);
     cudaMemcpy(dweights, hweights, (ll)(totalEdges) * sizeof(ll), cudaMemcpyHostToDevice);
@@ -285,6 +342,11 @@ int main(){
 
     if(algoChoice == 1) ssspVertexCentric(totalVertices, dindex, dheadVertex, dweights);
     else if(algoChoice == 2) ssspEdgeCentric(totalVertices, totalEdges, dsrc, dheadVertex, dweights);
+    else if(algoChoice == 3) ssspWorklist(totalVertices, dindex, dheadVertex, dweights);
+    else{
+        cout << "Invalid choice!" << endl;
+        return 0;
+    }
 
     size_t finalFreeMemory;
     cudaMemGetInfo(&finalFreeMemory, &totalMemory);
@@ -293,6 +355,147 @@ int main(){
     cout << "Consumed Memory: " << consumedMemory / (1024 * 1024) << " MB" << endl;
 
     return 0;
+}
+
+void ssspWorklist(ll totalVertices, ll *dindex, ll *dheadvertex, ll *dweights){
+    cudaEvent_t start, stop;
+    cudaEventCreate(&start);
+    cudaEventCreate(&stop);
+
+    float totalTime = 0.0;
+    float time;
+
+    ll *dist;
+    cudaMalloc(&dist, (ll)(totalVertices) *sizeof(ll));
+
+    ll srcVertex = 0;
+
+    unsigned int nodeblocks = ceil((double)totalVertices / (double)BLOCKSIZE);
+
+    time = 0.0;
+    cudaEventRecord(start);
+    ssspVertexInit<<<nodeblocks, BLOCKSIZE>>>(totalVertices, dist);
+    cudaEventRecord(stop);
+    cudaEventSynchronize(stop);
+
+    cudaEventElapsedTime(&time, start, stop);
+    totalTime += time;
+
+    cout << "Initialized distance array" << endl;
+    cout << endl;
+
+    float *workers = (float*)malloc(sizeof(float));
+//    cout << "done";
+    *workers = 1;
+
+    ll *curr;
+    cudaMalloc(&curr, totalVertices * sizeof(ll));
+
+    ll *next;
+    cudaMalloc(&next, totalVertices * sizeof(ll));
+
+    cout << "Initialized current worklist" << endl;
+    cout << endl;
+
+    float *idx;
+    cudaMalloc(&idx, sizeof(float));
+
+    cout << "Defined index for next worklist" << endl;
+    cout << endl;
+
+    time = 0.0;
+    cudaEventRecord(start);
+    init<<<1,1>>>(srcVertex, dist, curr);
+    cudaEventRecord(stop);
+    cudaEventSynchronize(stop);
+
+    cudaEventElapsedTime(&time, start, stop);
+    totalTime += time;
+
+    cout << "Initialized source distance and current worklist" << endl;
+    cout << endl;
+
+    ll itr = 1;
+
+    while(true){
+        unsigned blocks = ceil((double)(*workers) / BLOCKSIZE);
+        time = 0.0;
+        cudaEventRecord(start);
+        setIndexForWorklist<<<1, 1>>>(idx);
+        cudaEventRecord(stop);
+        cudaEventSynchronize(stop);
+
+        cudaEventElapsedTime(&time, start, stop);
+        totalTime += time;
+        cudaDeviceSynchronize();
+
+        cudaError_t err = cudaGetLastError();
+        if (err != cudaSuccess) {
+            printf("CUDA Error: %s\n", cudaGetErrorString(err));
+            return;
+        }
+
+//        cout << "Here";
+
+//        cout << "Before Kernel: " << endl;
+//        printDist<<<1,1>>>(*workers, curr);
+
+        if(itr % 2 != 0) {
+            time = 0.0;
+            cudaEventRecord(start);
+            ssspWorklistKernel<<<blocks, BLOCKSIZE>>>(*workers, dindex, dheadvertex, dweights, curr, next, dist, idx);
+            cudaEventRecord(stop);
+            cudaEventSynchronize(stop);
+
+            cudaEventElapsedTime(&time, start, stop);
+            totalTime += time;
+            cudaDeviceSynchronize();
+        }
+        else{
+            time = 0.0;
+            cudaEventRecord(start);
+            ssspWorklistKernel<<<blocks, BLOCKSIZE>>>(*workers, dindex, dheadvertex, dweights, next, curr, dist, idx);
+            cudaEventRecord(stop);
+            cudaEventSynchronize(stop);
+
+            cudaEventElapsedTime(&time, start, stop);
+            totalTime += time;
+            cudaDeviceSynchronize();
+        }
+
+        ++itr;
+
+        err = cudaGetLastError();
+        if (err != cudaSuccess) {
+            printf("CUDA Error1: %s\n", cudaGetErrorString(err));
+            return;
+        }
+
+        time = 0.0;
+        cudaEventRecord(start);
+        cudaMemcpy(workers, idx, sizeof(int), cudaMemcpyDeviceToHost);
+        cudaEventRecord(stop);
+        cudaEventSynchronize(stop);
+
+        cudaEventElapsedTime(&time, start, stop);
+        totalTime += time;
+
+        if(*workers == 0) break;
+
+        err = cudaGetLastError();
+        if (err != cudaSuccess) {
+            printf("CUDA Error: %s\n", cudaGetErrorString(err));
+            return;
+        }
+    }
+
+    cout << "Total Iterations: " << itr << endl;
+
+    cout << "First 10 values of dist vector: ";
+    printDist<<<1,1>>>(totalVertices, dist);
+    cudaDeviceSynchronize();
+
+    cout << "Total Time: " << totalTime << endl;
 }
 
 void ssspEdgeCentric(ll totalVertices, ll totalEdges, ll *src, ll *dest, ll *weights){
